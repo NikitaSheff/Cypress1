3e2cefaa1ed201bcab284f739c0a7575eb62648d
Shevchenko Nikita <nikitashevchenko@yandex.ru>
2022-06-30 19:31:44 +0300

    1

--- cypress/e2e/js-alerts.js
+++ cypress/e2e/js-alerts.js
@@ -4,9 +4,9 @@ describe("Handle JS alerts", ()=>
 {
     it("Confirm JS alert contains the correct text ", ()=>
     {
-        cy.visit("https://www.automationteststore.com/");
+        cy.visit("http://www.webdriveruniversity.com/");
         cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force: true})
-
+        cy.get('#button1').click()
     });
 
 
